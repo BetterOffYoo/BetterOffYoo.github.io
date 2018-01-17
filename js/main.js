d4fbb89c9a3ff47aa27bd09a06d731b18b86c43d
D <hdy1996001@163.com>
2018-01-18 00:15:53 +0800

    1
    1

--- js/main.js
+++
@@ -1,9 +0,0 @@
-function xiangxiClick() {
-    alert('没有介绍');
-}
-
-var typingText = document.getElementById('homeText');
-setTimeout(() => {
-    typingText.style.borderColor = 'transparent';
-    typingText.style.animationPlayState = 'paused';
-}, 6000);
